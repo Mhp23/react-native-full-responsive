c8aa29984f3912d366987dc438eeed1ee8e6d508
mhp23 <hpousty@gmail.com>
2024-01-04 16:22:48 +0330

    feat: adding withMediaQuery HOC

--- src/HOC/index.tsx
+++ src/HOC/index.tsx
@@ -1,25 +1,2 @@
-import React from 'react';
-import { useResponsiveMethods } from '../hooks';
-import type { ResponsiveMethodsProps } from '../types';
-/**
- * A HOC for when you want to use full responsive methods in your class components without additional steps as props.
- */
-const withResponsiveMethods = <T extends object>(
-  Component: React.ComponentType<T>
-) => {
-  const displayName = Component.displayName || Component.name || 'Component';
-
-  const ComponentWithMethods = (
-    props: Omit<T, keyof ResponsiveMethodsProps>
-  ) => {
-    const responsiveMethods = useResponsiveMethods();
-
-    return <Component {...(props as T)} {...responsiveMethods} />;
-  };
-
-  ComponentWithMethods.displayName = `withResponsiveMethods(${displayName})`;
-
-  return ComponentWithMethods;
-};
-
-export { withResponsiveMethods, withResponsiveMethods as withRM };
+export * from './withMediaQuery';
+export * from './withResponsiveMethods';
---
+++ src/HOC/withMediaQuery.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { useMediaQuery } from '../hooks';
+import type { DeviceType, MappedDeviceType } from '../types';
+/**
+ * A HOC for when you want to use full responsive methods in your class components without additional steps as props.
+ */
+const withMediaQuery = <T extends object>(
+  Component: React.ComponentType<T>,
+  thresholds?: Partial<MappedDeviceType>
+) => {
+  const displayName = Component.displayName || Component.name || 'Component';
+
+  const ComponentWithMethods = (props: { type?: DeviceType }) => {
+    const media = useMediaQuery(thresholds);
+    return <Component {...(props as T)} type={media} />;
+  };
+
+  ComponentWithMethods.displayName = `withMediaQuery(${displayName})`;
+
+  return ComponentWithMethods;
+};
+
+export { withMediaQuery };

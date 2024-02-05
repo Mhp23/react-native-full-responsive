918fc5d6599222ff8990670a82bc987f50a911d7
mhp23 <hpousty@gmail.com>
2024-02-05 22:35:27 +0330

    fix: lint issue on recursive mapping method

--- src/layout/createRStyle/mapping/recursiveMapping.ts
+++ src/layout/createRStyle/mapping/recursiveMapping.ts
@@ -1,9 +1,4 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import _ from 'lodash';
 import type { CreateStyleConfig } from '../../../types';
-
-const isObject = (value: unknown) =>
-  value !== null && typeof value === 'object';
 
 export const recursiveMapping = <T>(
   style: T,

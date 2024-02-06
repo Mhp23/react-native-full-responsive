028438eba99c3fefe50966bf83611aa4ec6a7c21
mhp23 <hpousty@gmail.com>
2024-02-06 11:50:22 +0330

    feat: adding recursive mapping method

--- src/layout/createRStyle/mapping/recursiveMapping.ts
+++ src/layout/createRStyle/mapping/recursiveMapping.ts
@@ -1,8 +1,26 @@
-import type { CreateStyleConfig } from '../../../types';
+import { parseValue } from '../parseValue';
+import type { CreateStyleConfig, ValuePattern } from '../../../types';
 
 export const recursiveMapping = <T>(
   style: T,
-  _styleConfig?: Partial<CreateStyleConfig>
+  styleConfig?: Partial<CreateStyleConfig>
 ) => {
-  return style;
+  const cache = new WeakMap();
+  const mapper = (currentStyle: unknown): unknown => {
+    if (typeof currentStyle !== 'object' || currentStyle === null) {
+      return parseValue(currentStyle as ValuePattern, styleConfig);
+    }
+    const cachedResult = cache.get(currentStyle);
+    if (cachedResult) return cachedResult;
+    const result = Array.isArray(currentStyle)
+      ? currentStyle.map(mapper)
+      : Object.create(null);
+    for (const property in currentStyle) {
+      //@ts-ignore
+      result[property] = mapper(currentStyle[property]);
+    }
+    cache.set(currentStyle, result);
+    return result;
+  };
+  return mapper(style);
 };

283d54f712bbd8ca4ee22cebadc2c5f892cab297
mhp23 <hpousty@gmail.com>
2025-05-27 21:46:20 +0330

    test: exporting issue in the jest file

--- jest/index.ts
+++ jest/index.ts
@@ -1,55 +1,84 @@
-export const getDimensions = jest.fn(() => ({
+const getDimensions = jest.fn(() => ({
   screenWidth: 375,
   screenHeight: 667,
 }));
 
-export const withMediaQuery = jest.fn(
-  (Component, _thresholds = {}) => Component
-);
-export const withMQ = withMediaQuery;
+const withMediaQuery = jest.fn((Component, _thresholds = {}) => Component);
+const withMQ = withMediaQuery;
 
-export const withResponsiveMethods = jest.fn((Component) => Component);
-export const withRM = withResponsiveMethods;
+const withResponsiveMethods = jest.fn((Component) => Component);
+const withRM = withResponsiveMethods;
 
-export const useDevice = jest.fn(() => ({ type: 'sm', bases: {} }));
+const useDevice = jest.fn(() => ({ type: 'sm', bases: {} }));
 
-export const useMediaQuery = jest.fn(() => 'sm');
-export const useMQ = useMediaQuery;
+const useMediaQuery = jest.fn(() => 'sm');
+const useMQ = useMediaQuery;
 
-export const useResponsiveDim = jest.fn(() => ({ width: 100, height: 100 }));
-export const useRD = useResponsiveDim;
+const useResponsiveDim = jest.fn(() => ({ width: 100, height: 100 }));
+const useRD = useResponsiveDim;
 
-export const useResponsiveScale = jest.fn(() => 1);
-export const useRS = useResponsiveScale;
+const useResponsiveScale = jest.fn(() => 1);
+const useRS = useResponsiveScale;
 
-export const useResponsiveWidth = jest.fn(() => 100);
-export const useRW = useResponsiveWidth;
+const useResponsiveWidth = jest.fn(() => 100);
+const useRW = useResponsiveWidth;
 
-export const useResponsiveHeight = jest.fn(() => 100);
-export const useRH = useResponsiveHeight;
+const useResponsiveHeight = jest.fn(() => 100);
+const useRH = useResponsiveHeight;
 
-export const useResponsiveMethods = jest.fn(() => ({
+const useResponsiveMethods = jest.fn(() => ({
   rs: jest.fn((n) => n),
   rw: jest.fn((n) => n),
   rh: jest.fn((n) => n),
 }));
-export const useRM = useResponsiveMethods;
+const useRM = useResponsiveMethods;
 
-export const FRContext = { Provider: jest.fn(), Consumer: jest.fn() };
-export const FRProvider = jest.fn(({ children }) => children);
+const FRContext = { Provider: jest.fn(), Consumer: jest.fn() };
+const FRProvider = jest.fn(({ children }) => children);
 
-export const responsiveScale = jest.fn((size) => size);
-export const rs = responsiveScale;
+const responsiveScale = jest.fn((size) => size);
+const rs = responsiveScale;
 
-export const responsiveWidth = jest.fn((widthPercentage) => widthPercentage);
-export const rw = responsiveWidth;
+const responsiveWidth = jest.fn((widthPercentage) => widthPercentage);
+const rw = responsiveWidth;
 
-export const responsiveHeight = jest.fn((heightPercentage) => heightPercentage);
-export const rh = responsiveHeight;
+const responsiveHeight = jest.fn((heightPercentage) => heightPercentage);
+const rh = responsiveHeight;
 
-export const createRStyle = jest.fn((style, _styleConfig = {}) => style);
+const createRStyle = jest.fn((style, _styleConfig = {}) => style);
 
-export const useRStyle = jest.fn((styles, _deps = []) => {
+const useRStyle = jest.fn((styles, _deps = []) => {
   const passedStyles = typeof styles === 'function' ? styles() : styles;
   return createRStyle(passedStyles);
 });
+
+export default {
+  getDimensions,
+  withMediaQuery,
+  withMQ,
+  withResponsiveMethods,
+  withRM,
+  useDevice,
+  useMediaQuery,
+  useMQ,
+  useResponsiveDim,
+  useRD,
+  useResponsiveScale,
+  useRS,
+  useResponsiveWidth,
+  useRW,
+  useResponsiveHeight,
+  useRH,
+  useResponsiveMethods,
+  useRM,
+  FRContext,
+  FRProvider,
+  responsiveScale,
+  rs,
+  responsiveWidth,
+  rw,
+  responsiveHeight,
+  rh,
+  createRStyle,
+  useRStyle,
+};
